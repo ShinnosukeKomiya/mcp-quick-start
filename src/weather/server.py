d366f943efe63a0cf312b68a56ac66c781016137
ShinnosukeKomiya <komiya.shinnosuke@lmi.ne.jp>
2024-12-30 14:08:02 +0900

    This defines two tools

---
+++ src/weather/server.py
@@ -0,0 +1,55 @@
+from typing import Any
+import asyncio
+import httpx
+from mcp.server.models import InitializationOptions
+import mcp.types as types
+from mcp.server import NotificationOptions, Server
+import mcp.server.stdio
+
+
+NWS_API_BASE = "https://api.weather.gov"
+USER_AGENT = "weather-app/1.0"
+
+server = Server("weather")
+
+
+@server.list_tools()
+async def handle_list_tools() -> list[types.Tool]:
+    """
+    ツールを列挙する。
+    各ツールは、JSONスキーマバリデーションを使って引数を指定する。
+    """
+    return [
+        types.Tool(
+            name="get-alerts",
+            description="Get weather alerts for a state",
+            inputSchema={
+                "type": "object",
+                "properties": {
+                    "state": {
+                        "type": "string",
+                        "description": "Two-letter state code (e.g. CA, NY)",
+                    },
+                },
+                "required": ["state"],
+            },
+        ),
+        types.Tool(
+            name="get-forecast",
+            description="Get weather forecast for a location",
+            inputSchema={
+                "type": "object",
+                "properties": {
+                    "latitude": {
+                        "type": "number",
+                        "description": "Latitude of the location",
+                    },
+                    "longitude": {
+                        "type": "number",
+                        "description": "Longitude of the location",
+                    },
+                },
+                "required": ["latitude", "longitude"],
+            },
+        ),
+    ]
